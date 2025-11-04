cba92f1ba6c30c69eda3189d05f4a4d7287bc332
Ivan Filippov <ivyfilippov@inno.tech>
2025-11-04 22:55:47 +0500

    add new logic for finding a files

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int fileCount = 0;
+        while (true) {
+            File file = new File(new Scanner(System.in).nextLine().trim());
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            //Проверяем, что это файл
+            if (fileExists && !isDirectory) {
+                fileCount++;
+                System.out.println("Путь указан верно\nЭто файл номер " + fileCount);
+                //Если не файл, проверяем, что это директория
+            } else if (isDirectory)
+                System.out.println("Это путь к директории");
+                //Если не файл и не директория, выводим уведомление об этом
+            else
+                System.out.println("Такого файла или директории не существует");
+        }
     }
 }
