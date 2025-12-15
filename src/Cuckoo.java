3d0b37182ccf975667e02efc452da248a5948c27
filippovI <filippovania66@gmail.com>
2025-12-15 23:40:14 +0500

    Delete src/Cuckoo.java

--- src/Cuckoo.java
+++
@@ -1,8 +0,0 @@
-public class Cuckoo extends Bird {
-    public void sign() {
-        int randomInt = (int) (Math.random() * 10) + 1;
-        for (int i = 0; i < randomInt; i++) {
-            System.out.println("ку-ку");
-        }
-    }
-}
