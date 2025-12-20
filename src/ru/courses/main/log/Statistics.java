fa74644b4d97df747e15deb589ba8f870819b75c
Ivan Filippov <ivyfilippov@inno.tech>
2025-12-21 03:38:40 +0500

    add existSites and operationSystemFrequency

--- src/ru/courses/main/log/Statistics.java
+++ src/ru/courses/main/log/Statistics.java
@@ -2,23 +2,36 @@ package ru.courses.main.log;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Objects;
 
 public class Statistics {
     private int totalTraffic;
     private LocalDateTime minTime;
     private LocalDateTime maxTime;
+    private final HashSet<String> existSites;
+    private final HashMap<String, Integer> operationSystemFrequency;
 
     public Statistics() {
         this.totalTraffic = 0;
         this.minTime = LocalDateTime.MAX;
         this.maxTime = LocalDateTime.MIN;
+        this.existSites = new HashSet<>();
+        this.operationSystemFrequency = new HashMap<>();
     }
 
     public void addEntry(LogEntry log) {
         this.totalTraffic += log.getDataSize();
         if (log.getTime().isBefore(this.minTime)) this.minTime = log.getTime();
         if (log.getTime().isAfter(this.maxTime)) this.maxTime = log.getTime();
+        if (log.getStatusCode() == 200) this.existSites.add(log.getPath());
+
+        String operationSystemName = log.getUserAgent().getOperationSystem();
+        if (!operationSystemFrequency.containsKey(operationSystemName))
+            operationSystemFrequency.put(operationSystemName, 1);
+        else
+            operationSystemFrequency.put(operationSystemName, operationSystemFrequency.get(operationSystemName + 1));
     }
 
     public double getTrafficRate() {
@@ -41,6 +54,23 @@ public class Statistics {
     public LocalDateTime getMaxTime() {
         return maxTime;
     }
+
+    public HashSet<String> getExistSites() {
+        return existSites;
+    }
+
+    public HashMap<String, Double> getOperationSystemFrequency() {
+        HashMap<String, Double> result = new HashMap<>();
+        int sumOS = operationSystemFrequency
+                .values()
+                .stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        operationSystemFrequency.forEach((k, v) -> result.put(k, (double) v / sumOS));
+        return result;
+    }
+
 
     @Override
     public boolean equals(Object o) {
